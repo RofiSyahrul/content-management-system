8509c1677bac0f3339a079dfefdd143f565a88c2
Rofi Syahrul <srofi44@gmail.com>
2019-10-14 21:31:29 +0700

    Modify 'get' and 'search' routers such that they can perform aggregation on data dates

--- server/routes/datadate.js
+++ server/routes/datadate.js
@@ -1,22 +1,67 @@
 const express = require("express");
 const router = express.Router();
 const Data = require("../models/datadate");
+const DataAggregation = require("../helper/data-aggregation");
+const aggregation = new DataAggregation(Data);
 
 // search
 router.post("/search", (req, res) => {
-  const filter = req.body;
-  Data.find(filter, (err, data) => {
-    if (err) console.error(err);
-    else if (data) res.status(200).json(data);
+  const filter = Object.keys(req.body).map(JSON.parse)[0];
+  const groupBy = req.header("GroupBy");
+  let limit = req.header("Limit");
+  let skip = Number(req.header("Skip") || 0);
+
+  aggregation.countData(groupBy, filter).then(result => {
+    let numOfPages = 0;
+    if (result[0]) {
+      if (limit == "all") limit = result[0].count;
+      limit = Number(limit || result[0].count);
+      numOfPages = Math.ceil((result[0].count || 0) / limit);
+      if (skip >= result[0].count) skip -= limit;
+    }
+    limit = Number(limit);
+
+    aggregation
+      .getData(groupBy, limit, skip, filter)
+      .then(data => {
+        if (groupBy) {
+          data = data.map(item => {
+            item.letter = item._id;
+            return item;
+          });
+        }
+        res.status(200).json({ numOfPages, data });
+      })
+      .catch(err => console.error(err));
   });
 });
 
 // get
 router.get("/", (req, res) => {
-  Data.find((err, data) => {
-    if (err) console.error(err);
-    else if (data) res.status(200).json(data);
-  });
+  const groupBy = req.header("GroupBy");
+  let limit = req.header("Limit");
+  let skip = Number(req.header("Skip") || 0);
+  aggregation
+    .countData(groupBy)
+    .then(result => {
+      if (limit == "all") limit = result[0].count;
+      limit = Number(limit || result[0].count);
+      const numOfPages = Math.ceil((result[0].count || 0) / limit);
+      if (skip >= result[0].count) skip -= limit;
+      aggregation
+        .getData(groupBy, limit, skip)
+        .then(data => {
+          if (groupBy) {
+            data = data.map(item => {
+              item.letter = item._id;
+              return item;
+            });
+          }
+          res.status(200).json({ numOfPages, data });
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
 });
 
 // edit
