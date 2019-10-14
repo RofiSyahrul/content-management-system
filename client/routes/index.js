aee88354a6532ddd40f80c908ceb2899b4e7413b
Rofi Syahrul <srofi44@gmail.com>
2019-10-14 10:55:59 +0700

    add route to render pie chart's page

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,14 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
 // display bar chart
-router.get('/bar', (req, res) => {
-  res.render('bar');
-})
+router.get("/bar", (req, res) => {
+  res.render("bar");
+});
+
+// display pie chart
+router.get("/pie", (req, res) => {
+  res.render("pie");
+});
 
 module.exports = router;
