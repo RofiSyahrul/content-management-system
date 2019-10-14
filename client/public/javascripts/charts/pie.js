544ae2cb009ea0b2a9eedb3c1600fd8499e9f009
Rofi Syahrul <srofi44@gmail.com>
2019-10-14 21:37:46 +0700

    Perform logic to get appropriate width of pie chart

--- client/public/javascripts/charts/pie.js
+++ client/public/javascripts/charts/pie.js
@@ -1,5 +1,5 @@
 let groupBy = localStorage.getItem("Group By") || "letter";
-let accumulator = localStorage.getItem("Accumulator") || "sumFreq";
+let accumulator = localStorage.getItem("Accumulator") || "countFreq";
 $(`#${accumulator}`).prop("checked", true);
 const accumulators = [
   { value: "maxFreq", label: "Maximum of Frequency" },
@@ -11,7 +11,7 @@ const accumulators = [
 
 const apiData = "http://localhost:3001/api/data";
 
-const getData = (groupBy, accumulator) => {
+const getData = (groupBy, accumulator, width) => {
   $.ajax({
     url: apiData,
     method: "GET",
@@ -21,11 +21,11 @@ const getData = (groupBy, accumulator) => {
       if (groupBy) req.setRequestHeader("GroupBy", groupBy);
     }
   }).done(response => {
-    showChart(response.data, accumulator);
+    showChart(response.data, accumulator, width);
   });
 };
 
-const showChart = (data, accumulator) => {
+const showChart = (data, accumulator, width) => {
   // Load the Visualization API and the corechart package.
   google.charts.load("current", { packages: ["corechart"] });
 
@@ -43,39 +43,42 @@ const showChart = (data, accumulator) => {
     data = new google.visualization.arrayToDataTable(data);
 
     // Set chart options
-    var options = {
+    let chartWidth = 700;
+    if (width < 576) chartWidth = "100%";
+    else if (width < 768) chartWidth = "90%";
+    const options = {
       title: `Frequency for Each Word`,
-      width: 700,
+      width: chartWidth,
       height: 400,
-      backgroundColor: 'transparent'
+      backgroundColor: "transparent"
       // legend: { position: "none" }
     };
 
     // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.PieChart($("#pieChart").get(0));
+    const chart = new google.visualization.PieChart($("#pieChart").get(0));
     chart.draw(data, options);
   }
 };
 
 $(() => {
-  getData(groupBy, accumulator);
+  let width = screen.width;
+  let height = screen.height;
+
+  getData(groupBy, accumulator, width);
   $("#freqOpt").change(() => {
     const checked = $(`[name="freq"]`)
       .toArray()
       .filter(item => item.checked)[0];
     accumulator = checked.getAttribute("value");
     localStorage.setItem("Accumulator", accumulator);
-    getData(groupBy, accumulator);
+    getData(groupBy, accumulator, width);
   });
-
-  let width = screen.width;
-  let height = screen.height;
 
   setInterval(() => {
     if (screen.width != width || screen.height != height) {
       width = screen.width;
       height = screen.height;
-      getData(groupBy, accumulator);
+      getData(groupBy, accumulator, width);
     }
   }, 100);
 
