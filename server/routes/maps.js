f66a2908d0dec41a7bedb9e7da8d9dba6b9884de
Rofi Syahrul <srofi44@gmail.com>
2019-10-16 21:31:17 +0700

    Add aggregation to perform limit and skip for pagination and update 'add' and 'edit' messages

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -1,22 +1,68 @@
 const express = require("express");
 const router = express.Router();
 const Maps = require("../models/map");
+const DataAggregation = require("../helper/data-aggregation");
+const aggregation = new DataAggregation(Maps, "title");
 
 // search
 router.post("/search", (req, res) => {
-  const filter = req.body;
-  Maps.find(filter, (err, data) => {
-    if (err) console.error(err);
-    else if (data) res.status(200).json(data);
+  const filter = Object.keys(req.body).map(JSON.parse)[0];
+  const groupBy = req.header("GroupBy");
+  let limit = req.header("Limit");
+  let skip = Number(req.header("Skip") || 0);
+
+  aggregation.countData(groupBy, filter).then(result => {
+    let numOfPages = 0;
+    if (result[0]) {
+      if (limit == "all") limit = result[0].count;
+      limit = Number(limit || result[0].count);
+      numOfPages = Math.ceil((result[0].count || 0) / limit);
+      if (skip >= result[0].count) skip -= limit;
+    }
+    limit = Number(limit);
+    if (limit.toString() == "NaN") limit = 1;
+
+    aggregation
+      .getData(groupBy, limit, skip, filter)
+      .then(data => {
+        if (groupBy) {
+          data = data.map(item => {
+            item.letter = item._id;
+            return item;
+          });
+        }
+        res.status(200).json({ numOfPages, data });
+      })
+      .catch(err => console.error(err));
   });
 });
 
 // get
 router.get("/", (req, res) => {
-  Maps.find((err, data) => {
-    if (err) console.error(err);
-    else if (data) res.status(200).json(data);
-  });
+  const groupBy = req.header("GroupBy");
+  let limit = req.header("Limit");
+  let skip = Number(req.header("Skip") || 0);
+  aggregation
+    .countData(groupBy)
+    .then(result => {
+      if (limit == "all") limit = result[0].count;
+      limit = Number(limit || result[0].count);
+      const numOfPages = Math.ceil((result[0].count || 0) / limit);
+      if (skip >= result[0].count) skip -= limit;
+      aggregation
+        .getData(groupBy, limit, skip)
+        .then(data => {
+          if (groupBy) {
+            data = data.map(item => {
+              item.letter = item._id;
+              return item;
+            });
+          }
+          res.status(200).json({ numOfPages, data });
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
 });
 
 // edit
@@ -44,7 +90,7 @@ router.post("/", (req, res) => {
       doc.__v = undefined;
       res.status(200).json({
         success: true,
-        message: "Data have been added.",
+        message: `${doc.title} have been added.`,
         data: doc
       });
     }
@@ -60,7 +106,7 @@ router.delete("/:id", (req, res) => {
       doc.__v = undefined;
       res.status(200).json({
         success: true,
-        message: "Data have been deleted.",
+        message: `${doc.title} have been deleted.`,
         data: doc
       });
     }
